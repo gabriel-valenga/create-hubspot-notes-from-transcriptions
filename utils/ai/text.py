2ab01c6475baf8d10dff0d826a2f55fad9640aa8
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2025-11-21 19:01:50 -0300

    feat: Implementation of the real Google AI Studio API KEY use in project, retrieving from aws parameter store

--- utils/ai/text.py
+++ utils/ai/text.py
@@ -4,7 +4,7 @@ from utils.aws.ssm.parameter_store import parameter_store
 class TextSummarizer():
 
     def __init__(self):
-        genai_api_key = parameter_store.get_parameter("GENAI_API_KEY")
+        genai_api_key = parameter_store.get_parameter("google_ai_studio_gen_api_key")
         genai.configure(api_key=genai_api_key)
         self.model = genai.GenerativeModel("models/gemini-2.5-flash-lite")
         self.chat = self.model.start_chat()
