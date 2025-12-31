c399160e1bd6abebfb999417dee5508f624b6b33
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2025-12-31 15:04:06 -0300

    test: improved the test coverage on test_aws_parameter_store.py

--- tests/utils/test_aws_ssm_parameter_store.py
+++ tests/utils/test_aws_ssm_parameter_store.py
@@ -1,8 +1,8 @@
 from unittest.mock import MagicMock
-from utils.aws.ssm.parameter_store import ParameterStore
+import utils.aws.ssm.parameter_store as parameter_store
 
 
-def test_get_parameter_success(monkeypatch):
+def test_get_parameter_success_using_env(monkeypatch):
     monkeypatch.setenv('TEST_KEY', 'secret-value')
     fake_client = MagicMock()
     fake_client.get_parameter.return_value = {
@@ -12,6 +12,24 @@ def test_get_parameter_success(monkeypatch):
         'utils.aws.ssm.parameter_store.boto3.client',
         lambda *_: fake_client
     )
-    ps = ParameterStore()
+    ps = parameter_store.ParameterStore()
     value = ps.get_parameter('TEST_KEY')
     assert value == 'secret-value'
+
+
+def test_get_parameter_success_using_aws_ssm(monkeypatch):
+    monkeypatch.setattr(parameter_store, "ENV", "prod")
+    fake_client = MagicMock()
+    fake_client.get_parameter.return_value = {
+        "Parameter": {"Value": "aws-secret"}
+    }
+    monkeypatch.setattr(parameter_store.boto3, "client", lambda *_: fake_client)
+    store = parameter_store.ParameterStore()
+    # clears lru_cache
+    store.get_parameter.cache_clear()
+    value = store.get_parameter("MY_KEY")
+    assert value == "aws-secret"
+    fake_client.get_parameter.assert_called_once_with(
+        Name="MY_KEY",
+        WithDecryption=True
+    )
