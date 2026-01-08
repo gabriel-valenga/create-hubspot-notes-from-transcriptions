ac32d7d7531ad4c5f5b93850733527a6f722d5ce
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2026-01-08 20:22:41 -0300

    feat: Created generel.py file in hubspot infra folder, to store general use hubspot infra code; Created functions to create a contact in hubspot filling in email, and a function to get a hubspot contact id by email

--- infra/hubspot/contacts.py
+++ infra/hubspot/contacts.py
@@ -1,1 +1,24 @@
+import requests
+from infra.hubspot.general import HUBSPOT_CRM_API_BASE_URL, HUBSPOT_CRM_API_HEADERS
+
 class HubspotContacts:
+
+    HUBSPOT_API_CONTACTS_URL = f'{HUBSPOT_CRM_API_BASE_URL}contacts/'
+
+    def get_a_contact_id_by_email(self, email:str):
+        url = f'{self.HUBSPOT_API_CONTACTS_URL}{email}?idProperty=email'
+        response = requests.get(url)
+        response.raise_for_status()
+        contact_id = response.json()['id']
+        return contact_id
+    
+
+    def create_hubspot_contact(self, email:str):
+        payload = {
+            'properties':{
+                'email': email
+            }
+        }
+        response = requests.post(self.HUBSPOT_API_CONTACTS_URL, json=payload, headers=HUBSPOT_CRM_API_HEADERS)
+        response.raise_for_status()
+        return response.json()['id']
---
+++ infra/hubspot/general.py
@@ -0,0 +1,5 @@
+HUBSPOT_CRM_API_BASE_URL = 'https://api.hubapi.com/crm/v3/objects/'
+HUBSPOT_CRM_API_HEADERS = {
+    "Authorization": "Bearer <token>",
+    "Content-Type": "application/json"
+}
