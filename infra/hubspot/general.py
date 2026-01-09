3431bc9f937233d3c0bbe9f435272cdab953b407
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2026-01-09 19:47:38 -0300

    feat: Created function to create hubspot note associated with a contact

--- infra/hubspot/general.py
+++ infra/hubspot/general.py
@@ -1,5 +1,10 @@
+from utils.aws.ssm.parameter_store import parameter_store
+
 HUBSPOT_CRM_API_BASE_URL = 'https://api.hubapi.com/crm/v3/objects/'
+HUBSPOT_INTEGRATION_PRIVATE_APP_KEY = parameter_store.get_parameter('HUBSPOT_INTEGRATION_PRIVATE_APP_KEY')
 HUBSPOT_CRM_API_HEADERS = {
-    "Authorization": "Bearer <token>",
+    "Authorization": HUBSPOT_INTEGRATION_PRIVATE_APP_KEY,
     "Content-Type": "application/json"
 }
+HUBSPOT_INTEGRATION_OWNER_ID = parameter_store.get_parameter('HUBSPOT_INTEGRATION_OWNER_ID')
+HUBSPOT_ASSOCIATION_TYPE_NOTE_TO_CONTACT = '202'
---
+++ infra/hubspot/notes.py
@@ -0,0 +1,37 @@
+import requests
+from infra.hubspot.general import (
+    HUBSPOT_CRM_API_BASE_URL, 
+    HUBSPOT_CRM_API_HEADERS,
+    HUBSPOT_INTEGRATION_OWNER_ID,
+    HUBSPOT_ASSOCIATION_TYPE_NOTE_TO_CONTACT
+)
+
+class HubspotNotes:
+
+    HUBSPOT_API_NOTES_URL = f'{HUBSPOT_CRM_API_BASE_URL}notes/'
+
+    def create_hubspot_note_associated_to_contact(self, note_text:str, id_contact:str):
+        payload = {
+            "properties": {
+                "hs_timestamp": "2021-11-12T15:48:22Z",
+                "hs_note_body": note_text,
+                "hubspot_owner_id": HUBSPOT_INTEGRATION_OWNER_ID,
+            },
+            "associations": [
+                {
+                    "to": {
+                        "id": id_contact
+                    },
+                    "types": [
+                        {
+                        "associationCategory": "HUBSPOT_DEFINED",
+                        "associationTypeId": HUBSPOT_ASSOCIATION_TYPE_NOTE_TO_CONTACT
+                        }
+                    ]
+                }
+            ]
+        }
+        response = requests.post(self.HUBSPOT_API_NOTES_URL, json=payload, headers=HUBSPOT_CRM_API_HEADERS)
+        response.raise_for_status()
+        return response.json()['id']
+    
