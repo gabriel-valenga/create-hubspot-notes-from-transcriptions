1a413db45b1c838ef8eb792886de55d1c0a56c13
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2026-01-31 13:58:52 -0300

    tests: Created unit test cases for infra/hubspot/contacts file

---
+++ tests/infra/hubspot/test_contacts.py
@@ -0,0 +1,29 @@
+import pytest
+from infra.hubspot.contacts import HubspotContacts
+from tests.mocks.mock_hubspot_api import MockHubspotApi
+
+mock_hubspot_api = MockHubspotApi()
+hubspot_contacts = HubspotContacts()
+
+@pytest.fixture
+def mock_hubspot_api_contacts_get_success(monkeypatch):
+    def fake_get(*args, **kwargs):
+        return mock_hubspot_api.return_an_object_with_success()
+    monkeypatch.setattr("infra.hubspot.contacts.requests.get", fake_get)
+
+
+@pytest.fixture
+def mock_hubspot_api_contacts_post_created(monkeypatch):
+    def fake_post(*args, **kwargs):
+        return mock_hubspot_api.return_a_created_object()
+    monkeypatch.setattr("infra.hubspot.contacts.requests.post", fake_post)
+
+
+def test_get_a_contact_by_email_success(mock_hubspot_api_contacts_get_success):    
+    contact_id = hubspot_contacts.get_a_contact_id_by_email(email='test@email.com')
+    assert contact_id == '1'
+
+
+def test_create_hubspot_contact(mock_hubspot_api_contacts_post_created):
+    contact_id = hubspot_contacts.create_hubspot_contact(email='testnew@email.com')
+    assert contact_id == '1'
